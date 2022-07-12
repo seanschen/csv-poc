65bdd4e6ffb9f6b2d8314424ee3679a7f85432c6
Sean Chen <seanschen1@gmail.com>
2022-07-12 14:02:47 -0700

    Added oneschema importer embed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import {flatfileImporter} from "@flatfile/sdk";
+import OneSchemaImporter from "@oneschema/react"
+import { useState } from "react"
 
 const divStyle = {
     margin: "2px",
@@ -9,9 +11,31 @@ const divStyle = {
     height: "5%",
     width: "25%"
 };
+const oneSchemaStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+}
 
 function App() {
+    const [isOpen, setIsOpen] = useState(false)
+
     const importer = flatfileImporter("");
+
+    const onSuccess = (data) => {
+        // TODO handle success
+    }
+
+    const onCancel = () => {
+        // TODO handle cancel
+    }
+
+    const onError = (message) => {
+        // TODO handle error
+    }
+
 
     return (
         <div className="App">
@@ -35,8 +59,25 @@ function App() {
             </div>
             <div style={divStyle}>
                 <p>OneSchema</p>
-                <a href="https://www.loom.com/share/379ff48b08244c93a8791292846160c7">Link to Demo Video</a>
+                <button onClick={() => setIsOpen(true)}>Import</button>
             </div>
+            <OneSchemaImporter
+                /* managing state from your application */
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+                /* required config values */
+                clientId="9bc874db-13b8-46ea-a642-7cafbfc5b6f7"
+                userJwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiPFVTRVJfSUQ-IiwiaXNzIjoiOWJjODc0ZGItMTNiOC00NmVhLWE2NDItN2NhZmJmYzViNmY3In0.ljfztcF94hlz-0xoIJvLCNP5jRz7rFLTaoNC-BvOBoQ"
+                templateKey="employee_metadata"
+                /* optional config values */
+                blockImportIfErrors={true}
+                devMode={true}
+                style={oneSchemaStyle}
+                /* handling results */
+                onSuccess={onSuccess}
+                onCancel={onCancel}
+                onError={onError}
+            />
         </div>
     );
 }
