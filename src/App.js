79f62b1b0834083f2aae59c228f0245fa481204f
Sean Chen <seanschen1@gmail.com>
2022-08-23 00:07:01 -0700

    Some changes

--- src/App.js
+++ src/App.js
@@ -23,9 +23,11 @@ function App() {
     const [isCapTableOpen, setIsCapTableOpen] = useState(false)
     const [isPerformanceOpen, setIsPerformanceOpen] = useState(false)
     const [isPayrollOpen, setIsPayrollOpen] = useState(false)
+    const [isEmploymentMetadataOpen, setIsEmploymentMetadataOpen] = useState(false)
 
     const clientId = "9bc874db-13b8-46ea-a642-7cafbfc5b6f7";
     const userJwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiPFVTRVJfSUQ-IiwiaXNzIjoiOWJjODc0ZGItMTNiOC00NmVhLWE2NDItN2NhZmJmYzViNmY3In0.ljfztcF94hlz-0xoIJvLCNP5jRz7rFLTaoNC-BvOBoQ";
+    const webhookKey = 'sean-local';
 
     const onSuccess = (data) => {
         // TODO handle success
@@ -55,7 +57,7 @@ function App() {
                 userJwt={userJwt}
                 templateKey="benchmarks"
                 /* optional config values */
-                webhookKey="dev"
+                webhookKey={webhookKey}
                 config={{
                     blockImportIfErrors: true,
                 }}
@@ -78,7 +80,7 @@ function App() {
                 userJwt={userJwt}
                 templateKey="cap_table"
                 /* optional config values */
-                webhookKey="dev"
+                webhookKey={webhookKey}
                 config={{
                     blockImportIfErrors: true,
                 }}
@@ -101,7 +103,30 @@ function App() {
                 userJwt={userJwt}
                 templateKey="performance"
                 /* optional config values */
-                webhookKey="dev"
+                webhookKey={webhookKey}
+                config={{
+                    blockImportIfErrors: true,
+                }}
+                style={oneSchemaStyle}
+                /* handling results */
+                onSuccess={onSuccess}
+                onCancel={onCancel}
+                onError={onError}
+            />
+            <div style={divStyle}>
+                <p>Payroll</p>
+                <button onClick={() => setIsPayrollOpen(true)}>Import</button>
+            </div>
+            <OneSchemaImporter
+                /* managing state from your application */
+                isOpen={isPayrollOpen}
+                onRequestClose={() => setIsPayrollOpen(false)}
+                /* required config values */
+                clientId={clientId}
+                userJwt={userJwt}
+                templateKey="payroll"
+                /* optional config values */
+                webhookKey={webhookKey}
                 config={{
                     blockImportIfErrors: true,
                 }}
@@ -113,18 +138,18 @@ function App() {
             />
             <div style={divStyle}>
                 <p>Employment Data</p>
-                <button onClick={() => setIsPayrollOpen(true)}>Import</button>
+                <button onClick={() => setIsEmploymentMetadataOpen(true)}>Import</button>
             </div>
             <OneSchemaImporter
                 /* managing state from your application */
-                isOpen={isPayrollOpen}
-                onRequestClose={() => setIsPayrollOpen(false)}
+                isOpen={isEmploymentMetadataOpen}
+                onRequestClose={() => setIsEmploymentMetadataOpen(false)}
                 /* required config values */
                 clientId={clientId}
                 userJwt={userJwt}
                 templateKey="employment_data"
                 /* optional config values */
-                webhookKey="dev"
+                webhookKey={webhookKey}
                 config={{
                     blockImportIfErrors: true,
                 }}
